02785a03d623e83b8a2e0ed7c1da1bebf98f54fc
Daksh Grover <csecec.1802019@gmail.com>
2020-06-30 16:04:13 +0530

    Update Check_Login.java

--- src/AuthenticatingUser/Check_Login.java
+++ src/AuthenticatingUser/Check_Login.java
@@ -10,13 +10,12 @@ public class Check_Login {
 
 	public boolean checklogin(LoginBean loginbean) throws Exception {
 		try {
-			// response.setHeader("Cache-control", "no-cache,no-store,must-revalidate");
 			String uname = loginbean.getUname();
 			String pass = loginbean.getPass();
 			String course = loginbean.getCourse();
 			Connection con = ConnectionProvider.provideConnection();
 			String query = "select * from " + course + " where uname=?";
-                        System.out.println(query);
+                        //System.out.println(query);
 			PreparedStatement st = con.prepareStatement(query);
 			st.setString(1, uname);
 			ResultSet rs = st.executeQuery();
